169d1167de9905a13e23deee25fbf3ace75515dd
TriPineTree <38550486+TriPineTree@users.noreply.github.com>
2019-12-04 16:28:32 -0800

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,12 @@
 #include <opencv2/core/utility.hpp>
+#include "opencv2/core/core.hpp"
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp" 
 #include <iostream>
+
+#define PI 3.14159265
 
 /*********************************************
 Improvements suggestions:
@@ -153,18 +156,20 @@ Vec3b convolute(const Mat& input, const Mat& kernel, int oRow, int oCol)
 // calcEllipse
 // Pre-condition : row, col are non-negative integers
 // Post-condition : returns a value based on the ellipse mathematical equation
-// ellipse equation
-	// (x - h)^2 / a^2 + (y - k)^2 / b^2 = 1
-	// x is col
-	// y is row
-	// h, k is the ellipse x, y center
-	// a is the x-axis radius
-	// b is the y-axis radius
-double calcEllipse(int row, int col, Point center, int width, int height)
+// ellipse equation with rotation
+	// ((X-Cx)*cos⁡(θ)+(Y-Cy)*sin⁡(θ))^2/(Rx)^2 +
+	// ((X-Cx)*sin⁡(θ)-(Y-Cy)*cos⁡(θ))^2/(Ry)^2 =1
+	// (Cx,Cy)  is the center of the Ellipse.
+ 	// Rx is the Major-Radius, and Ry is the Minor-Radius.
+	// θ is the angle of the Ellipse rotation.
+
+double calcEllipse(int row, int col, Point center, int width, int height, double angle = 0)
 {
 	double leftHandSide1, leftHandSide2, ans;
-	leftHandSide1 = pow((col - center.x), 2) / pow((width / 2), 2);
-	leftHandSide2 = pow((row - center.y), 2) / pow((height / 2), 2);
+	leftHandSide1 = pow((col - center.x) * cos(angle * PI / 180) + (row - center.y) * 
+		sin(angle * PI / 180), 2) / pow((width/2), 2);
+	leftHandSide2 = pow((col - center.x) * sin(angle * PI / 180) - (row - center.y) * 
+		cos(angle * PI / 180), 2) / pow((height/2), 2);
 	ans = leftHandSide1 + leftHandSide2;
 
 	return ans;
@@ -174,7 +179,7 @@ double calcEllipse(int row, int col, Point center, int width, int height)
 // 
 // Pre-condition:
 // Post-condition:
-Mat blurCorner(const Mat& input, int width, int height, Point center) {
+Mat blurCorner(const Mat& input, int width, int height, Point center, double angle) {
 	Mat output = input.clone();
 
 	//Make blur kernel
@@ -188,53 +193,17 @@ Mat blurCorner(const Mat& input, int width, int height, Point center) {
 	}; */
 
 	double kernelVals[49] = { 0.000036, 0.000363, 0.001446, 0.002291, 0.001446, 0.000363, 0.000036, 0.000363,
-0.003676,
-0.014662,
-0.023226,
-0.014662,
-0.003676,
-0.000363,
-0.001446,
-0.014662,
-0.058488,
-0.092651,
-0.058488,
-0.014662,
-0.001446,
-0.002291,
-0.023226,
-0.092651,
-0.146768,
-0.092651,
-0.023226,
-0.002291,
-0.001446,
-0.014662,
-0.058488,
-0.092651,
-0.058488,
-0.014662,
-0.001446,
-0.000363,
-0.003676,
-0.014662,
-0.023226,
-0.014662,
-0.003676,
-0.000363,
-0.000036,
-0.000363,
-0.001446,
-0.002291,
-0.001446,
-0.000363,
-0.000036};
+					0.003676,0.014662,0.023226,0.014662,0.003676,0.000363,0.001446,0.014662,0.058488,0.092651,
+					0.058488,0.014662,0.001446,0.002291,0.023226,0.092651,0.146768,0.092651,0.023226,0.002291,
+					0.001446,0.014662,0.058488,0.092651,0.058488,0.014662,0.001446,0.000363,0.003676,0.014662,
+					0.023226,0.014662,0.003676,0.000363,0.000036,0.000363,0.001446,0.002291,0.001446,0.000363,
+					0.000036 };
 	Mat blurKernel = Mat(7, 7, DataType<double>::type, kernelVals);
 
 	for (int row = 0; row < output.rows; row++) {
 		for (int col = 0; col < output.cols; col++) {
 
-			double LHS = calcEllipse(row, col, center, width, height);
+			double LHS = calcEllipse(row, col, center, width, height,angle);
 
 			//if at border blur
 			if (LHS < 1.05 && LHS > 0.8) {
@@ -244,7 +213,6 @@ Mat blurCorner(const Mat& input, int width, int height, Point center) {
 
 		}
 	}
-	displayImage(output, "bruh");
 	return output;
 }
 
@@ -297,7 +265,7 @@ Mat overlay(const Mat& face, const Mat& input) {
 		}
 	}
 
-	imwrite("Overlay.jpg", output);
+	cv::imwrite("Overlay.jpg", output);
 
 	return output;
 }
@@ -312,14 +280,14 @@ Mat overlay(const Mat& face, const Mat& input) {
 //		- int width: the widht of the face
 //		- int height: the height of the face (chin to hairtop)
 //		- Point center: the center point of the face (around the nose)
-Mat fetchFace(const Mat& input, int width, int height, Point center) {
+Mat fetchFace(const Mat& input, int width, int height, Point center, double angle) {
 
 	Mat onlyFace = input.clone();
 
 	for (int row = 0; row < onlyFace.rows; row++) {
 		for (int col = 0; col < onlyFace.cols; col++) {
 
-			double LHS = calcEllipse(row, col, center, width, height);
+			double LHS = calcEllipse(row, col, center, width, height,angle);
 
 			// makes it green if outside the head
 			if (LHS > 1)
@@ -333,12 +301,12 @@ Mat fetchFace(const Mat& input, int width, int height, Point center) {
 /** @function detectAndDisplay */
 Mat detectAndDisplay(const Mat& frame)
 {
-	double scaleFactor = 1.6; 
-	cout << "width: " << frame.cols << "height: " << frame.rows << endl;
+	double scaleFactor = 1.6;
+	std::cout << "width: " << frame.cols << "height: " << frame.rows << endl;
 	Point finalCenter;
-	Mat jerFace;
-	Mat enlargedJer;
+	Mat faceOnly;
 	Mat frame_gray;
+
 	Mat overlayed = frame.clone();
 	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
 	equalizeHist(frame_gray, frame_gray);
@@ -349,81 +317,63 @@ Mat detectAndDisplay(const Mat& frame)
 	face_cascade.detectMultiScale(frame_gray, faces);
 
 	for (auto val : faces)
-		cout << val << " ";
+		std::cout << val << " ";
 
-	cout << "Number of faces detected : " << faces.size();
-	cout << endl;
+	std::cout << "Number of faces detected : " << faces.size() << endl;
 
 	// saves the center,width and height of each faces
 	vector<pair<Point, pair<double, double>>> facePositions(faces.size());
 
-	for (size_t i = 0; i < faces.size(); i++)
+	Point eye0, eye1;
+	double angle = 0;
+	Mat output = frame.clone();
+
+	// For each face
+	for (int i = 0; i < faces.size(); i++)
 	{
 
+		//Get eyes information
+		Mat faceROI = frame_gray(faces[i]);
+		vector<Rect> eyes;
+		eyes_cascade.detectMultiScale(faceROI, eyes);
+		std::cout << "No. of eyes: " << eyes.size() << endl;
+		if (eyes.size() == 2)
+		{
+			eye0 = Point(eyes[0].x, eyes[0].y);
+			eye1 = Point(eyes[1].x, eyes[1].y);
+			angle = atan((double)(eye0.y - eye1.y) / (double)(eye0.x - eye1.x)) * 180 / PI;
+			std::cout << "Angle is: " << angle << endl;
+		}
+
+		// Get face information
 		int cx = faces[i].x + faces[i].width / 2;
 		int cy = faces[i].y + faces[i].height / 2 - faces[i].height * 0.08;
-
-		// Point object of the face's center
 		Point center(cx, cy);
-		finalCenter = center;
+		Point faceCenter(cx, cy);
 		
-		facePositions[i] = make_pair(finalCenter, make_pair(faces[i].width * scaleFactor * 0.9, faces[i].height * 1.4 * scaleFactor));
-
-		//ellipse(frame, center, Size(faces[i].width / 2, (faces[i].height / 2) * 1.3), 0, 0, 360, Scalar(255, 0, 255), 8);
-
-		Point topLeft(faces[i].x, faces[i].y);
-		cout << topLeft << endl;
-		Point bottomRight(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
-
-		// rectangle(frame, topLeft, bottomRight, Scalar(255, 0, 255), 8, 0, 0);
-
-		Mat faceROI = frame_gray(faces[i]);
-
-		//-- In each face, detect eyes
-		std::vector<Rect> eyes;
-		eyes_cascade.detectMultiScale(faceROI, eyes);
-
-
+		// Drawing ellipse
+		Size faceSize(faces[i].width, (faces[i].height) * 1.3);
+		RotatedRect rRect(faceCenter, faceSize, angle);
+		ellipse(output, rRect, Scalar(0, 255, 0), 5);
 
 		// An image with the cropped face with a green background
-		jerFace = fetchFace(frame, faces[i].width * 0.9, faces[i].height * 1.4, center);
+		faceOnly = fetchFace(frame, faces[i].width * 0.9, faces[i].height * 1.4, center, angle);
 
 		// An image with the enlarged cropped face with a green background
-		enlargedJer = scale(jerFace, cx, cy, scaleFactor);
-
-		overlayed = overlay(enlargedJer, overlayed);
-
-		//Circling the eye, do we need this? -- yes i think we should show the mid steps of detecting faces - Donghee 
-
-		for (size_t j = 0; j < eyes.size(); j++)
-		{
-			Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-			int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
-			circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
-
-			cout << "eye for face #" << i << ": " << eye_center << endl;
-
-
-		}
-		displayImage(frame, "eye detected");
-
+		faceOnly = scale(faceOnly, cx, cy, scaleFactor);
+		overlayed = overlay(faceOnly, overlayed);
 	}
 
-
-	//Mat overlayed = overlay(enlargedJer, frame);
-	
 	// for loop to blur all edges of all faces
-	imwrite("beforeBlur.jpg", overlayed);
-	Mat output = overlayed.clone();
+	output = overlayed.clone();
 	for (int i = 0; i < faces.size(); i++) {
-		output = blurCorner(output, facePositions[i].second.first, facePositions[i].second.second, facePositions[i].first);
+		output = blurCorner(output, facePositions[i].second.first, 
+		facePositions[i].second.second, facePositions[i].first, angle);
 	}
 
-	//Mat output = blurCorner(overlayed, faces[0].width * scaleFactor, faces[0].height * 1.3 * scaleFactor, finalCenter);
-
-	//displayImage(output, "blurred Corners");
+	displayImage(output, "blurred Corners");
 	imwrite("afterBlur.jpg", output);
-	return enlargedJer;
+	return output;
 }
 
 // displayImage
@@ -440,12 +390,14 @@ void displayImage(const Mat& img, String winName)
 
 
 int main(int argc, const char** argv) {
-	std::cout << "runs" << endl;
+	cout << "runs" << endl;
 
-	Mat original = imread("fourFaces.jpg");
+	String filename = argv[1];
+	cout << filename << endl;
+	Mat original = imread(filename);
 	while (original.rows * original.cols >= 500000) //more than 500 x 500
 		resize(original, original, Size(), 0.5, 0.5);
-	
+
 
 	//-- 1. Load the cascades
 	if (!face_cascade.load(face_cascade_name)) { printf("--(1)Error loading\n"); return -1; };
@@ -456,8 +408,6 @@ int main(int argc, const char** argv) {
 	imwrite("original.jpg", original);
 	Mat enlarged = detectAndDisplay(original); //this changed original fo some reason 
 	imwrite("originalAfterEnlarged.jpg", original);
-
-	//Mat ans = detectAndDisplay(original);
 
 
 	//Mat overlayed = overlay(enlarged, original);
